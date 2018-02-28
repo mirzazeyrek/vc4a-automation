dfebdd6b1c6f8e7068d64016dc5bc8bb86d0fcb4
uğur mirza zeyrek <github@mersinuni.net>
2018-02-28 14:52:59 +0300

    added commit message.

--- branch-syncher.sh
+++ branch-syncher.sh
@@ -9,18 +9,18 @@ echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to 
 
 case $CURRENT_BRANCH in
 production)
-  git checkout master && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
-  git checkout staging && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
-  git checkout development && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
+  git checkout master && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   git checkout $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
   ;;
 master)
-  git checkout staging && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
-  git checkout development && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
+  git checkout staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   git checkout $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
   ;;
 staging)
-  git checkout development && git merge $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   git checkout $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
   ;;
 esac
