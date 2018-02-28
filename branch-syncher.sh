695dbb9d112441931d3342cda94f475a7d57a7bf
uğur mirza zeyrek <github@mersinuni.net>
2018-02-28 14:26:35 +0300

    added branch syncher.

---
+++ branch-syncher.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Automatically merge the last commit through the following branches:
+# production -> master -> staging -> development
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LAST_COMMIT=$(git rev-list -1 HEAD)
+
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to sub-branches
+
+case $CURRENT_BRANCH in
+production)
+  git checkout master ＆＆ git merge $CURRENT_BRANCH
+  git checkout staging ＆＆ git merge $CURRENT_BRANCH
+  git checkout development ＆＆ git merge $CURRENT_BRANCH
+  git checkout $CURRENT_BRANCH
+  ;;
+master)
+  git checkout staging ＆＆ git merge $CURRENT_BRANCH
+  git checkout development ＆＆ git merge $CURRENT_BRANCH
+  git checkout $CURRENT_BRANCH
+  ;;
+staging)
+  git checkout development ＆＆ git merge $CURRENT_BRANCH
+  git checkout $CURRENT_BRANCH
+  ;;
+esac
