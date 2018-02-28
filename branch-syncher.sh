29eac44b39398bd8b66266d3e2e4be27c1547e89
uğur mirza zeyrek <github@mersinuni.net>
2018-02-28 16:34:09 +0300

    removed -b

--- branch-syncher.sh
+++ branch-syncher.sh
@@ -10,24 +10,24 @@ CURRENT_BRANCH=$1
 LAST_COMMIT=$(git rev-list -1 HEAD)
 
 git remote set-url origin git@github.com:billz/mu-plugins.git
-git checkout -b $CURRENT_BRANCH
+git checkout $CURRENT_BRANCH
 echo "Fetching all"
 git fetch --all
 
 echo "Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to sub-branches"
 case $CURRENT_BRANCH in
 production)
-  git checkout -b master && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
-  git checkout -b staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
-  git checkout -b development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout master && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   ;;
 master)
-  git checkout -b staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
-  git checkout -b development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   ;;
 staging)
-  git checkout -b development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
+  git checkout development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" || ( echo "auto merge failed." && exit 1 )
   ;;
 esac
 
-git checkout -b $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
+git checkout $CURRENT_BRANCH || ( echo "auto merge failed." && exit 1 )
